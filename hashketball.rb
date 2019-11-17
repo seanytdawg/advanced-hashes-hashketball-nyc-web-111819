8bbc865778a2db7f914e48c11d945202f9fa6254
Sean Tarzy <seantarzy@gmail.com>
2019-11-17 02:09:04 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -128,18 +128,158 @@ def game_hash
 end 
   # Write your code here!
 def num_points_scored(player_name)
-  if game_hash[:home][:players].include?(player_name)
- return game_hash[:home][:players][player_name][:points]
-else return game_hash[:away][:players][player_name][:points]
+  game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+      if player[:player_name] == player_name 
+        return player[:points]
+      end
+    end 
+  end 
 end 
+
+def shoe_size(player_name)
+  game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+      if player[:player_name] == player_name 
+        return player[:shoe]
+      end
+    end 
+  end 
+end 
+
+def team_colors(team_name)
+  game_hash.each do |home_or_away, team_values|
+    if team_values[:team_name] == team_name 
+      return team_values[:colors]
+    end 
+  end 
 end 
   
+  def team_names
+    new_array = []
+  game_hash.each do |home_or_away, team_values|
+  new_array << team_values[:team_name]
+    end 
+  return new_array
+end 
+  
+  def player_numbers(team_name)
+    new_array = []
+    game_hash.each do |home_or_away, team_values|
+      if team_values[:team_name] == team_name
+      team_values[:players].each do |player|
+        new_array << player[:number]
+      end 
+    end 
+  end 
+    return new_array
+  end 
+      
+  
+def player_stats(player_name)
+game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+      if player[:player_name] == player_name 
+        player.delete(:player_name)
+        return player
+      end 
+    end 
+  end 
+end 
+
+def big_shoe_rebounds 
+   game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+      if player[:shoe] == 19 
+        return player[:rebounds]
+      end 
+    end 
+  end 
+end 
+
+def most_points_scored 
+  point_array = []
+   game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+        point_array << player[:points]
+      end 
+    end 
+  sorted_point_array = point_array.sort 
+  game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+        if player[:points] == sorted_point_array[-1]
+          return player[:player_name]
+        end 
+      end 
+    end 
+end 
+  def player_with_longest_name
+  point_array = []
+   game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+        point_array << player[:player_name].length 
+      end 
+    end 
+  sorted_point_array = point_array.sort 
+  game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+        if player[:player_name].length == sorted_point_array[-1]
+          return player[:player_name]
+        end 
+      end 
+    end 
+end 
+  
+   def team_point_total(hash)
+        team_points = 0 
+         hash[:players].each do |player|
+       team_points += player[:points]
+    end 
+     return team_points
+     end 
+     
+def winning_team
+  home_point_total = team_point_total(game_hash[:home])
+  away_point_total = team_point_total(game_hash[:away])
+      if home_point_total > away_point_total
+        return game_hash[:home][:team_name]
+      else 
+        return game_hash[:away][:team_name]
+      end 
+    end 
+
+def most_steals
+  point_array = []
+   game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+        point_array << player[:steals]
+      end 
+    end 
+  sorted_point_array = point_array.sort 
+  game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+        if player[:steals] == sorted_point_array[-1]
+          return player[:player_name]
+        end 
+      end 
+    end 
+  end 
+
+def long_name_steals_a_ton?
+  player_with_longest_name == most_steals
+end 
 
 
-
-
-
-
-
-
-
+def most_points_scored 
+  player_name = ""
+  max_points = 0 
+   game_hash.each do |home_or_away, team_values|
+      team_values[:players].each do |player|
+        if  player[:points] > max_points 
+          max_points = player[:points]
+          player_name = player[:player_name]
+       end 
+     end 
+    end 
+          return player_name
+end 
